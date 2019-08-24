79fc4cfd668f5bbdf2d4cffbedc6b0a7f0788dfb
Julia-Agasaro <agasarojulia@gmail.com>
2019-08-24 12:08:35 +0200

    second commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,4 +7,8 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'Quotes';
+  quotes:string[];
+  constructor(){
+    this.quotes = ['The Way Get Started Is To Quit Talking And Begin Doing', 'The Pessimist Sees Difficulty In Every Opportunity. The Optimist Sees Opportunity In Every Difficulty.', 'You Learn More From Failure Than From Success. Don’t Let It Stop You. Failure Builds Character.','Don’t Let Yesterday Take Up Too Much Of Today.']
+  } 
 }
