36cc2a4c179a30ac4cfd1d5e273d143ab386aca5
Julia-Agasaro <agasarojulia@gmail.com>
2019-08-25 18:03:31 +0200

    Added an image

--- src/app/quote/quote.component.ts
+++ src/app/quote/quote.component.ts
@@ -7,7 +7,7 @@ import { Quote } from '../quote';
 })
 export class QuoteComponent implements OnInit {
   quotes:Quote [] = [
-    new Quote(1,'The Way Get Started Is To Quit Talking And Begin Doing', 'Walt Disney','Julia Agasaro', new Date(2019,8,25)),
+    new Quote(1,'The Way To Get Started Is To Quit Talking And Begin Doing', 'Walt Disney','Julia Agasaro', new Date(2019,8,23)),
     new Quote(2,'You Learn More From Failure Than From Success. Don’t Let It Stop You. Failure Builds Character.','Unknown','Naice Uwitonze', new Date(2019,8,25)),
     new Quote(3,'Don’t Let Yesterday Take Up Too Much Of Today.', 'Will Rogers','Eliane Kamana', new Date(2019,8,25)),
     new Quote(4,'It’s Not Whether You Get Knocked Down, It’s Whether You Get Up.', 'Vince Lombardi', 'Will Smith', new Date(2019,8,25)),
