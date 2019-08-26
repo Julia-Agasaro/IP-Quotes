410f0fe171d52c54d0dea8513310722572f69cb6
Julia-Agasaro <agasarojulia@gmail.com>
2019-08-26 08:49:33 +0200

    Edited Quote detail.html

--- src/app/date-count.pipe.ts
+++ src/app/date-count.pipe.ts
@@ -1,10 +1,10 @@
-import { Pipe, PipeTransform } from '@angular/core';
-
+import { Pipe, PipeTransform,Input } from '@angular/core';
+import{Quote} from '@angular/compiler'
 @Pipe({
   name: 'dateCount'
 })
 export class DateCountPipe implements PipeTransform {
-
+@Input() quote:Quote;
   transform(value: any): number {
     let today:Date = new Date(); //get current date and time
     let todayWithNoTime:any = new Date(today.getFullYear(), today.getMonth(), today.getDate())
